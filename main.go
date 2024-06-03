c5ea8d8be3ec371acec9851db17ee678f214490c
Noah <fg5678910@yahoo.com.tw>
2024-06-03 13:46:17 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(Cors())
+	router.GET("/test", test)
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 是否存取cookie
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		method := c.Request.Method
+		// 放行OPTIONS方法
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+		}
+		// 處理請求
+		c.Next()
+	}
+}
+
+func test(g *gin.Context) {
+	g.JSON(200, struct {
+		Status uint16      `json:"status"`
+		Msg    string      `json:"msg"`
+		Data   interface{} `json:"data"`
+	}{
+		Status: 200,
+		Msg:    "ok",
+		Data:   nil,
+	})
+}
