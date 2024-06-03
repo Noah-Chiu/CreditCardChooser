b8a812534ca575efc5bc842cac472d8f5f736752
Noah <fg5678910@yahoo.com.tw>
2024-06-03 17:01:09 +0800

    perf response function

--- main.go
+++ main.go
@@ -1,14 +1,54 @@
 package main
 
 import (
+	"bytes"
 	"credit-card-chooser/util"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+type WebhookEvent struct {
+	Type            string          `json:"type"`
+	Timestamp       int64           `json:"timestamp"`
+	Source          Source          `json:"source"`
+	ReplyToken      string          `json:"replyToken,omitempty"`
+	Mode            string          `json:"mode"`
+	WebhookEventID  string          `json:"webhookEventId"`
+	DeliveryContext DeliveryContext `json:"deliveryContext"`
+	Message         Message         `json:"message"`
+}
+
+type Source struct {
+	Type   string `json:"type"`
+	UserID string `json:"userId"`
+}
+
+type DeliveryContext struct {
+	IsRedelivery bool `json:"isRedelivery"`
+}
+
+type Message struct {
+	Type       string `json:"type"`
+	ID         string `json:"id"`
+	QuoteToken string `json:"quoteToken"`
+	Text       string `json:"text"`
+}
+
+type WebhookPayload struct {
+	Destination string         `json:"destination"`
+	Events      []WebhookEvent `json:"events"`
+}
+
+type Reply struct {
+	ReplyToken string    `json:"replyToken"`
+	Messages   []Message `json:"messages"`
+}
 
 func main() {
 	router := gin.Default()
@@ -43,7 +83,7 @@ func Cors() gin.HandlerFunc {
 }
 
 func ReceiveData(g *gin.Context) {
-	request := util.WebhookPayload{}
+	request := WebhookPayload{}
 
 	g.ShouldBind(&request)
 
@@ -52,7 +92,8 @@ func ReceiveData(g *gin.Context) {
 	fmt.Println(req)
 
 	for _, event := range request.Events {
-		fmt.Println(event.Message.Text)
+		fmt.Println("收到的訊息:", event.Message.Text)
+		event.Response()
 	}
 
 	g.JSON(200, struct {
@@ -64,4 +105,44 @@ func ReceiveData(g *gin.Context) {
 		Msg:    "ok",
 		Data:   req,
 	})
+}
+
+func (e *WebhookEvent) Response() {
+	var buf bytes.Buffer
+	res := Reply{}
+
+	msg := "hello, this is test"
+	res.ReplyToken = e.ReplyToken
+	res.Messages = append(res.Messages, Message{
+		Type:       "text",
+		Text:       msg,
+		QuoteToken: e.Message.QuoteToken,
+	})
+
+	fmt.Println("回傳的訊息:", msg)
+
+	json.NewEncoder(&buf).Encode(res)
+
+	req, err := http.NewRequest("POST", util.ReplyUrl, &buf)
+	if err != nil {
+		return
+	}
+
+	// Set the request content type
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer {%s}", util.Token))
+
+	// Send the HTTP request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	sitemap, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println("收到的回傳:", string(sitemap))
+
+	// gjson.Get(string(sitemap), "")
 }
