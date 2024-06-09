deb5c7f6b2c82c20809dc5add0ae07deedef49b3
Noah <fg5678910@gmail.com>
2024-06-10 02:19:24 +0800

    perf multiple partners

--- chooser.go
+++ chooser.go
@@ -62,76 +62,139 @@ func (event *WebhookEvent) chooseCard() {
 	res.ReplyToken = event.ReplyToken
 
 	text := strings.Split(event.Message.Text, " ")
-	partner, country := "", ""
+	inPartner, country := "", ""
 	if len(text) > 1 {
 		country = text[1]
 	}
-	partner = text[0]
+	inPartner = text[0]
 
-	// 取得所有卡別
-	db.Debug().Order(`"d_rewards"`).Find(&cardsData)
-	rewardsType := "國內消費"
-	bestCardInfo := ""
-	secondCardInfo := ""
-	max := [2]float64{}
+	// 取得所有相對應的商家
+	diffPartners := []string{}
+	// 尋找合作商家
+	db.Debug().
+		Where(`"partner" ilike ?`, "%"+inPartner+"%").
+		Order(`"rewards" desc`).
+		Group(`"partner"`).
+		Find(&diffPartners)
 
-	// 跑過所有卡別
-	for _, card := range cardsData {
-		// --------------------------國外消費--------------------------
-		// 非台灣就是國外
-		if country != "" && country != "台灣" && country != "臺灣" && strings.ToUpper(country) != "TW" && strings.ToUpper(country) != "TAIWAN" {
-			rewardsType = fmt.Sprintf("國外消費(%s)", country)
-			// 吉鶴卡只有日本有優惠
-			if card.CardNo == "002" {
-				if country != "日本" && strings.ToUpper(country) != "JP" && strings.ToUpper(country) != "JAPAN" {
-					card.ORewards = 1
+	if len(diffPartners) == 0 {
+		// 取得所有卡別
+		db.Debug().Order(`"d_rewards"`).Find(&cardsData)
+
+		rewardsType := "國內消費"
+		bestCardInfo := ""
+		secondCardInfo := ""
+		max := [2]float64{}
+
+		// 跑過所有卡別
+		for _, card := range cardsData {
+			// --------------------------國外消費--------------------------
+			// 非台灣就是國外
+			if country != "" && country != "台灣" && country != "臺灣" && strings.ToUpper(country) != "TW" && strings.ToUpper(country) != "TAIWAN" {
+				rewardsType = fmt.Sprintf("國外消費(%s)", country)
+				// 吉鶴卡只有日本有優惠
+				if card.CardNo == "002" {
+					if country != "日本" && strings.ToUpper(country) != "JP" && strings.ToUpper(country) != "JAPAN" {
+						card.ORewards = 1
+					}
 				}
+				if card.ORewards >= max[0] {
+					secondCardInfo = bestCardInfo
+					bestCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%\n備註: %s", card.CardNm, card.ORewards, card.Note)
+					max[1] = max[0]
+					max[0] = card.ORewards
+				}
+				continue
 			}
-			if card.ORewards >= max[0] {
+
+			// --------------------------國內消費--------------------------
+			totalRewards := card.DRewards
+			if totalRewards >= max[0] {
 				secondCardInfo = bestCardInfo
-				bestCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%\n備註: %s", card.CardNm, card.ORewards, card.Note)
+				bestCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%", card.CardNm, totalRewards)
+
 				max[1] = max[0]
-				max[0] = card.ORewards
+				max[0] = totalRewards
+			} else if totalRewards >= max[1] {
+				secondCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%", card.CardNm, totalRewards)
+
+				max[1] = totalRewards
 			}
-			continue
 		}
 
-		// --------------------------國內消費--------------------------
-
-		partnersData := partners{}
-		addonRewards := 0.0
-		note := ""
-		// 尋找合作商家
-		result := db.Debug().
-			Where(`"card_no" = ? AND "partner" ilike ?`, card.CardNo, "%"+partner+"%").
-			Order(`"rewards" desc`).
-			Limit(1).
-			Find(&partnersData)
-		if result.RowsAffected > 0 {
-			rewardsType = fmt.Sprintf("國內消費(%s)", partnersData.Partner)
-			addonRewards = partnersData.Rewards
-			note = partnersData.Note
+		msg := Message{
+			Type:       "text",
+			Text:       rewardsType + fmt.Sprintf("\n1.\n%s\n2.\n%s", bestCardInfo, secondCardInfo),
+			QuoteToken: event.Message.QuoteToken,
 		}
-		totalRewards := card.DRewards + addonRewards
-		if totalRewards >= max[0] {
-			secondCardInfo = bestCardInfo
-			bestCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%\n備註: %s", card.CardNm, totalRewards, note)
-
-			max[1] = max[0]
-			max[0] = totalRewards
-		} else if totalRewards >= max[1] {
-			secondCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%\n備註: %s", card.CardNm, totalRewards, note)
-
-			max[1] = totalRewards
-		}
+		res.Messages = append(res.Messages, msg)
 	}
 
-	msg := Message{
-		Type:       "text",
-		Text:       rewardsType + fmt.Sprintf("\n1.\n%s\n2.\n%s", bestCardInfo, secondCardInfo),
-		QuoteToken: event.Message.QuoteToken,
+	for _, partner := range diffPartners {
+		// 取得所有卡別
+		db.Debug().Order(`"d_rewards"`).Find(&cardsData)
+
+		rewardsType := fmt.Sprintf("國內消費(%s)", partner)
+		bestCardInfo := ""
+		secondCardInfo := ""
+		max := [2]float64{}
+
+		// 跑過所有卡別
+		for _, card := range cardsData {
+			// --------------------------國外消費--------------------------
+			// 非台灣就是國外
+			if country != "" && country != "台灣" && country != "臺灣" && strings.ToUpper(country) != "TW" && strings.ToUpper(country) != "TAIWAN" {
+				rewardsType = fmt.Sprintf("國外消費(%s)", country)
+				// 吉鶴卡只有日本有優惠
+				if card.CardNo == "002" {
+					if country != "日本" && strings.ToUpper(country) != "JP" && strings.ToUpper(country) != "JAPAN" {
+						card.ORewards = 1
+					}
+				}
+				if card.ORewards >= max[0] {
+					secondCardInfo = bestCardInfo
+					bestCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%\n備註: %s", card.CardNm, card.ORewards, card.Note)
+					max[1] = max[0]
+					max[0] = card.ORewards
+				}
+				continue
+			}
+
+			// --------------------------國內消費--------------------------
+			partnersData := partners{}
+			addonRewards := 0.0
+			note := ""
+			// 尋找合作商家
+			result := db.Debug().
+				Where(`"card_no" = ? AND "partner" = ?`, card.CardNo, partner).
+				Order(`"rewards" desc`).
+				Limit(1).
+				Find(&partnersData)
+			if result.RowsAffected > 0 {
+				addonRewards = partnersData.Rewards
+				note = partnersData.Note
+			}
+			totalRewards := card.DRewards + addonRewards
+			if totalRewards >= max[0] {
+				secondCardInfo = bestCardInfo
+				bestCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%\n備註: %s", card.CardNm, totalRewards, note)
+
+				max[1] = max[0]
+				max[0] = totalRewards
+			} else if totalRewards >= max[1] {
+				secondCardInfo = fmt.Sprintf("卡別: %s\n總回饋: %.1f%%\n備註: %s", card.CardNm, totalRewards, note)
+
+				max[1] = totalRewards
+			}
+		}
+
+		msg := Message{
+			Type:       "text",
+			Text:       rewardsType + fmt.Sprintf("\n1.\n%s\n2.\n%s", bestCardInfo, secondCardInfo),
+			QuoteToken: event.Message.QuoteToken,
+		}
+		res.Messages = append(res.Messages, msg)
 	}
-	res.Messages = append(res.Messages, msg)
 
 	fmt.Println("回傳的訊息:", res.Messages)
 
